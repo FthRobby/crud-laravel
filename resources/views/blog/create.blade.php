e8bf7cb12d5d1c7c3cda361e139df0fec141b514
FthRobby <Rbysalam10@gmail.com>
2021-09-14 17:44:52 +0700

    edit code

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -7,7 +7,7 @@
     <title>Tambah Data</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-<body style="background: lightgray">
+<body class="bg-dark">
 
     <div class="container mt-5 mb-5">
         <div class="row">
